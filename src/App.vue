921b84ba5685d5889a41101f9b3a6e2b07e93ff2
hynever <hynever@163.com>
2026-02-25 22:44:14 +0800

    init project & tailwindcss & element-plus

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p class="text-3xl font-bold text-blue-600 underline">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <el-button icon="Plus" type="primary">新增</el-button>
+</template>
+
+<style scoped></style>
