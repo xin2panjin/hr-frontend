c8aee36859988d72ad0d1e8d9ff9bb2e593de8be
hynever <hynever@163.com>
2026-02-26 11:16:08 +0800

    add user store

---
+++ src/stores/user.ts
@@ -0,0 +1,73 @@
+import { defineStore } from 'pinia'
+
+export interface Department {
+  id: string
+  name: string
+}
+
+export interface User {
+  id: string
+  username: string
+  email: string
+  phone_number: string | null
+  realname: string
+  department: Department
+  is_active: boolean
+  is_superuser: boolean
+  is_hr: boolean
+  created_at: string
+  managed_departments?: Department[] // HR负责的部门列表
+}
+
+export interface UserState {
+  user: User | null
+  accessToken: string | null
+}
+
+export const useUserStore = defineStore('user', {
+  state: (): UserState => {
+    // 数据是存储在浏览器的localStorage中的，所以在初始化时需要从localStorage中读取数据
+    const user = localStorage.getItem('user')
+    const accessToken = localStorage.getItem('accessToken')
+    return {
+      user: user ? JSON.parse(user) : null,
+      accessToken: accessToken || null,
+    }
+  },
+
+  getters: {
+    isLoggedIn(): boolean {
+      return !!this.accessToken
+    },
+    getUserInfo(): User | null {
+      return this.user
+    },
+    getAccessToken(): string | null {
+      return this.accessToken
+    },
+    isHr(): boolean {
+      return this.user?.is_hr || false
+    },
+    isSuperUser(): boolean {
+      return this.user?.is_superuser || false
+    },
+  },
+
+  actions: {
+    login(user: User, accessToken: string) {
+      this.user = user
+      this.accessToken = accessToken
+      // 将用户信息和访问令牌保存到localStorage中
+      localStorage.setItem('user', JSON.stringify(user))
+      localStorage.setItem('accessToken', accessToken)
+    },
+
+    logout() {
+      this.user = null
+      this.accessToken = null
+      // 从localStorage中清除用户信息和访问令牌
+      localStorage.removeItem('user')
+      localStorage.removeItem('accessToken')
+    },
+  },
+})
