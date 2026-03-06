67d132ffbe8ec6450e436b166457efcc90507f67
hynever <hynever@163.com>
2026-03-06 14:10:21 +0800

    build

---
+++ dist/assets/index-CXv1eZtT.js
@@ -0,0 +1,1 @@
+import{_ as n,o,i as r,e as t}from"./index-DT3WPh8r.js";const s={};function a(c,e){return o(),r("div",null,[...e[0]||(e[0]=[t("h1",null,"设置",-1)])])}const _=n(s,[["render",a]]);export{_ as default};
